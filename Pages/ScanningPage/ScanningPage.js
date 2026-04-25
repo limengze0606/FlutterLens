366a990e8cea0e61464fbe150dac7e37ba1701fb
thomas <kage910606@gmail.com>
2026-04-25 21:42:16 +0800

    feat: 改成使用後置鏡頭

--- Pages/ScanningPage/ScanningPage.js
+++ Pages/ScanningPage/ScanningPage.js
@@ -7,8 +7,15 @@ function drawScanningPage() {
 }
 
 function startCamera() {
-  // 啟動 p5.js 相機，並在成功讀取後切換到掃描狀態
-  video = createCapture(VIDEO, function() {
+  let constraints = {
+    video: {
+      facingMode: "environment" // 關鍵字：強制要求使用後置(環境)鏡頭
+    },
+    audio: false // 確保不要求麥克風權限
+  };
+
+  // 啟動 p5.js 相機，並傳入我們設定的條件
+  video = createCapture(constraints, function() {
     currentPagesState = PagesState.SCANNING;
   });
   
